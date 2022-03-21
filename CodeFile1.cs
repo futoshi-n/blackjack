122ff1875788bc9987bbb81650df5886b6649f72
futoshi-n <aksxbc@gmail.com>
2022-03-22 00:37:12 +0900

    CardNo

--- CodeFile1.cs
+++ CodeFile1.cs
@@ -16,6 +16,7 @@ namespace SoloLearn
             Console.WriteLine("hello");
 
             Deck d = new Deck();
+            Console.WriteLine(d.CardNo(23));
             // Console.WriteLine(d.card[0].No);
             /*
             Card card =new Card();
@@ -85,12 +86,13 @@ namespace SoloLearn
     }
     public class Deck
     {
+        public List<Card> card;
         public Deck()
         {
             string[] marks = new string[] { "ハート", "スペード", "クラブ", "ダイヤ" };
 
-            List<Card> card = new List<Card>();
-
+            card = new List<Card>();
+            
             foreach (string mark in marks)
             {
                 for (int i = 1; i <= 13; i++)
@@ -100,12 +102,11 @@ namespace SoloLearn
                 }
             }
         }
-       // public Card CardNo(int i)
-       // {
-
-           // int t = this.card[i].No;
-            //return t;
-       // }
+        
+          public int CardNo(int i)
+          {
+           return card[i].No;
+          }
     }
 }
 
